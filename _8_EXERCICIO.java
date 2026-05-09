4409e65724b82035259b61af3d962b280b00bdd3
Paulo César <pcns029@gmail.com>
2026-05-09 17:54:43 -0300

    Update _8_EXERCICIO.java

--- _8_EXERCICIO.java
+++ _8_EXERCICIO.java
@@ -1,13 +1,23 @@
-package Lista10_JavaFlowgorithm;
-
 import java.util.Scanner;
 
-public class _8_EXERCICIO {
+public class TrocaMetades {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] vetorA = new int[10];
-        int[] vetorB = new int[10];
+        Scanner leitor = new Scanner(System.in);
+        int[] vetor = new int[12];
 
+        for (int i = 0; i < 12; i++) {
+            System.out.print("Posição " + i + ": ");
+            vetor[i] = leitor.nextInt();
+        }
 
+        // Troca os elementos (0 com 6, 1 com 7, etc)
+        for (int i = 0; i < 6; i++) {
+            int temp = vetor[i];
+            vetor[i] = vetor[i + 6];
+            vetor[i + 6] = temp;
+        }
+
+        System.out.print("\nVetor Resultante: ");
+        for (int num : vetor) System.out.print(num + " ");
     }
 }
